26c1a7519778016e4418290125cea9bf134c9007
Nghiep <asta@Nghieps-MacBook-Pro.local>
2020-05-11 23:45:44 +0700

    [Fix] Connect database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,11 @@
 import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'locahost',
+    host: '127.0.0.1',
     port: 5432,
     username: 'postgres',
     password: '12345678',
     database: 'qr_code',
-    entities: [__dirname + '/../**/*.entity.ts'],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 }
